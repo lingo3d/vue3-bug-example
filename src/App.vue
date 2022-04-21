a47094e3056af923e50e244d11a279e8bb1c67ac
lingosandi <91113613+lingosandi@users.noreply.github.com>
2022-04-21 23:01:11 +0800

    code uploaded

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { onUnmounted, ref } from "vue";
+import Test from "./components/Test.vue"
+
+const counter = ref(0)
+
+const interval = setInterval(() => {
+  counter.value++
+}, 500)
+
+onUnmounted(() => {
+  clearInterval(interval)
+})
+
+</script>
+
+<template>
+<Test :message="'hello ' + counter">
+  <Test :message="'world ' + counter" />
+</Test>
+</template>
---
+++ src/components/Test.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+const props = defineProps({
+    message: String
+})
+</script>
+
+<template>
+    <div>{{props.message}}</div>
+    <slot />
+</template>
